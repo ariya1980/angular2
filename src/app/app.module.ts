35cc7f17635588e4a5c8a80a21e0c80874d9eaca
Ariya_klu <kungpoo8>
2017-10-10 09:24:05 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductService } from './product.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [HttpClient, ProductService],
+  bootstrap: [AppComponent]
+  //bootstrap: [ProductListComponent]
+})
+export class AppModule { }
